984dd903eae08616536934906f82c52034168ef5
Robert Dixon <79385125+robsd@users.noreply.github.com>
2021-11-10 00:14:24 +0000

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 length = document.getElementById('length');
 displayLength = document.getElementById('display-length');
+includeLowercase = document.getElementById('include-lowercase');
 includeUppercase = document.getElementById('include-uppercase');
 includeNumbers = document.getElementById('include-numbers');
 includeSymbols = document.getElementById('include-symbols');
@@ -9,43 +10,31 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 symbols = ['!', '@', '£', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '[', ']', '{', '}', ';', ':', '\'', '"', '\\', '|', '`', '~', ',', '<', '.', '>', '/', '?'];
 
-generatePassword();
-
 length.oninput = function() {
 	displayLength.innerHTML = length.value;
-	generatePassword();
-}
-
-includeUppercase.oninput = function() {
-	generatePassword();
-}
-
-includeNumbers.oninput = function() {
-	generatePassword();
-}
-
-includeSymbols.oninput = function() {
-	generatePassword();
 }
 
 function generatePassword() {
-	password = "";
-	while (password.length < length.value) {
-		if (password.length < length.value) {
-			password += letters[Math.floor(Math.random() * letters.length)];
+	if (includeLowercase.checked || includeUppercase.checked || includeNumbers.checked || includeSymbols.checked)
+	{
+		password = "";
+		while (password.length < length.value) {
+			if (password.length < length.value && includeLowercase.checked) {
+				password += letters[Math.floor(Math.random() * letters.length)];
+			}
+			if (password.length < length.value && includeUppercase.checked) {
+				password += letters[Math.floor(Math.random() * letters.length)].toUpperCase();
+			}
+			if (password.length < length.value && includeNumbers.checked) {
+				password += numbers[Math.floor(Math.random() * numbers.length)];
+			}
+			if (password.length < length.value && includeSymbols.checked) {
+				password += symbols[Math.floor(Math.random() * symbols.length)];
+			}
 		}
-		if (password.length < length.value && includeUppercase.checked) {
-			password += letters[Math.floor(Math.random() * letters.length)].toUpperCase();
-		}
-		if (password.length < length.value && includeNumbers.checked) {
-			password += numbers[Math.floor(Math.random() * numbers.length)];
-		}
-		if (password.length < length.value && includeSymbols.checked) {
-			password += symbols[Math.floor(Math.random() * symbols.length)];
-		}
+		password = password.split('').sort(function() {
+			return 0.5 - Math.random();
+		}).join('');
+		displayPassword.value = password;
 	}
-	password = password.split('').sort(function() {
-		return 0.5 - Math.random();
-	}).join('');
-	displayPassword.value = password;
 }
